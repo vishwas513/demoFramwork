44bdbcec93186d6f17da2f078b04369e5bf05e59
Vishwas Mukund <vishwas.mukund@mobileconsultingsolutions.com>
2020-01-02 08:14:37 -0500

    Initial Commit

---
+++ ddf/ddf.h
@@ -0,0 +1,19 @@
+//
+//  ddf.h
+//  ddf
+//
+//  Created by MCS on 1/2/20.
+//  Copyright © 2020 MCS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ddf.
+FOUNDATION_EXPORT double ddfVersionNumber;
+
+//! Project version string for ddf.
+FOUNDATION_EXPORT const unsigned char ddfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ddf/PublicHeader.h>
+
+
