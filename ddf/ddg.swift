0db6a6d82b93be35a5cc4c743ae0b91c54d76dbe
Vishwas Mukund <vishwas.mukund@mobileconsultingsolutions.com>
2020-01-02 09:19:07 -0500

    changes

---
+++ ddf/ddg.swift
@@ -0,0 +1,17 @@
+//
+//  ddg.swift
+//  ddf
+//
+//  Created by MCS on 1/2/20.
+//  Copyright © 2020 MCS. All rights reserved.
+//
+
+import UIKit
+
+public class ddg {
+    
+   public static func capitizeGI(input: String) -> String {
+        return input.uppercased()
+    }
+    
+}
